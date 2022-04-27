5a97ac40a3203171170f8e599d75ff9ed8e2e003
Jose Testa <josetesta7@gmail.com>
2022-04-27 14:41:08 -0400

    Included javascript animation "Expend you mind!" on the Aside, did some css finetuning on the Home page.

--- home.js
+++ home.js
@@ -1,4 +1,31 @@
-//alert('Hello Greek Learners!');
-//alert('May all be to the glory of Christ!');
+alert('Welcome Greek Learners! Please consider sending us your feedback at the contact page. Thank you!');
 
-console.log ("Hey Zorba!");
+
+//console.log ("Hey Zorba!");
+
+
+
+
+//function validateForm() {
+    //console.log("Hello World!");}
+
+    const animation=document.querySelector("#animation")
+
+animation.addEventListener("mouseenter", () => {
+    if(!animation.classList.contains("hover")) {
+        animation.classList.add("hover");
+    }
+})
+
+animation.addEventListener("mouseleave", () => {
+    if(animation.classList.contains("hover")) {
+    animation.classList.remove("hover");
+    }
+})
+
+
+
+
+
+
+
